62b239bd4a282484e0705fb8eb08a6e2e350de38
lynn <872617417@qq.com>
2026-06-23 14:46:48 +0800

    fix

--- js/config/constants.js
+++ js/config/constants.js
@@ -20,7 +20,7 @@ export const SHOW_LABELS = false;
 export const DEFAULT_BRIGHTNESS = 0.74;
 
 export const STRATEGIC_LABEL_ITEMS = [
-  { key: 'base', text: '以价值三角为基', position: { x: 49.8, y: 76.6 } },
+  { key: 'base', text: '以价值三角为基', position: { x: 49.55, y: 76.6 } },
   { key: 'axis', text: '以战略中轴为路', position: { x: 33.8, y: 39.4 } },
   { key: 'slices', text: '以战略课题为门', position: { x: 65.6, y: 39.7 } },
   { key: 'apex', text: '战略目标', position: { x: 49.2, y: 5.9 } }
